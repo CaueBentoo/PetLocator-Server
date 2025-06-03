e535e061abc390d432eba8d72ceadb98b378487c
cauelop <cauebento29@gmail.com>
2025-06-02 21:26:26 -0300

    Ajusta porta para usar variável de ambiente no backend

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const jwt = require("jsonwebtoken");
 const path = require("path");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
 
